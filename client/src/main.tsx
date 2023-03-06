1d4c2d568dd9c4bbfe2fa22faabe0693f9593596
Kaio Silveira <kaionpsa@gmail.com>
2023-03-06 14:12:34 -0300

    basic structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
+import { GoogleOAuthProvider } from "@react-oauth/google"
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <GoogleOAuthProvider clientId={`${import.meta.env.VITE_GOOGLE_AUTH_TOKEN}`}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 )
