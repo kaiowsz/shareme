81d03c8c15a670c70d0d70d31e022539f75f64f3
Kaio Silveira <kaionpsa@gmail.com>
2023-03-07 09:22:59 -0300

    creating feed layout

--- client/src/container/Pins.tsx
+++ client/src/container/Pins.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components";
 
-const Pins = () => {
+const Pins = ({user}) => {
+  const [searchTerm, setSearchTerm] = useState("")
+
+  console.log(user)
+
   return (
-    <div>Pins</div>
+    <main className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar user={user} searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route path="/pin-detail/:pinId" element={<PinDetail user={user} />} />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route path="/search" element={<Search searchTerm={searchTerm} setSearchTerm={searchTerm} />} />
+        </Routes> 
+      </div>
+
+    </main>
   )
 }
 
--- client/src/utils/data.ts
+++ client/src/utils/data.ts
@@ -3,3 +3,34 @@ export const userQuery = (userId: string) => {
 
     return query
 }
+
+export const searchQuery = (searchTerm: string) => {
+    const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match ]{
+        image {
+            asset -> {
+                url
+            }
+        },
+        _id,
+        destination,
+        postedBy -> {
+            _id,
+            username,
+            image,
+        },
+        save[] {
+            _key,
+            postedBy -> {
+                _id,
+                username,
+                image
+            },
+        },
+    }`
+
+    return query
+}
+
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+    
+}`
