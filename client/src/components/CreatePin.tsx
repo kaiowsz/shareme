f0e8cfbc0e92b7b24315e5c41957ac8d8ed20b5d
Kaio Silveira <kaionpsa@gmail.com>
2023-03-07 16:12:35 -0300

    adding image to sanity feature

--- client/src/components/CreatePin.tsx
+++ client/src/components/CreatePin.tsx
@@ -47,6 +47,39 @@ const CreatePin = ({user}: any) => {
     }
   }
 
+  const savePin = () => {
+    if(title && about && destination && imageAsset?._id && category) {
+      const doc = {
+        _type: "pin",
+        title,
+        about,
+        destination,
+        image: {
+          _type: "image",
+          asset: {
+            _type: "reference",
+            _ref: imageAsset?._id
+          }
+        },
+        userId: user._id,
+        postedBy: {
+          _type: "postedBy",
+          _ref: user._id,
+        },
+        category,
+      }
+
+      client.create(doc)
+        .then(() => {
+          navigate("/")
+        })
+    } else {
+      setFields(true);
+
+      setTimeout(() => setFields(false), 2000)
+    }
+  }
+
 
   return (
     <div className="flex flex-col justify-center items-center mt-5 lg: h-4/5">
@@ -98,6 +131,51 @@ const CreatePin = ({user}: any) => {
             placeholder="Add your title here"
             className="outline-none text-2xl sm:text-3xl font-bold border-b-2 border-gray-200 p-2"
             />
+            {user && (
+              <div className="flex gap-2 my-2 items-center bg-white rounded-lg">
+                <img src={user.image} alt="user profile" className="w-10 h-10 rounded-full" />
+                <p className="font-bold">{user.username}</p>
+              </div>
+            )}
+            <input 
+            type="text"
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+            placeholder="What is your pin about"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+            />
+            <input 
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder="Add your destination link"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+            />
+            <div className="flex flex-col">
+              <div>
+                <p className="mb-2 font-semibold text-lg sm:text-xl">Choose Pin Category</p>
+                <select 
+                onChange={(e) => setCategory(e.target.value)}
+                className="outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+                name="" id="">
+                  <option value="other" className="bg-white">Select Category</option>
+                  {categories.map((category: any) => (
+                    <option className="text-base border-0 outline-none capitalize bg-white text-black" value={category.name}>{category.name}</option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="flex justify-end items-end mt-5">
+                  <button
+                  type="button"
+                  onClick={savePin}
+                  className="bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none"
+                  >
+                    Save Pin
+                  </button>
+              </div>
+
+            </div>
         </div>
 
       </div>
