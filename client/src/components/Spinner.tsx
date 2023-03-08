aafaf0eb3a11054bc46d161750d8d67eae1ed3a6
Kaio Silveira <kaionpsa@gmail.com>
2023-03-08 08:42:46 -0300

    profile page

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Circles } from "react-loader-spinner"
+import { ColorRing } from "react-loader-spinner"
 
 interface Spinner {
   message: string;
 }
 
+// color="#00BFFF"
+
 const Spinner = ({message}: Spinner) => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      <Circles color="#00BFFF" height={50} width={200} wrapperClass="m-5" />
+      <ColorRing height={50} width={200} wrapperClass="m-5" />
 
       <p className="text-lg text-center px-2">{message}</p>
     </div>
