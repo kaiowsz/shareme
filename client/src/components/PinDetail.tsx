2421dac4cf94eb7b5bea99da30b47f1422280ab5
Kaio Silveira <kaionpsa@gmail.com>
2023-03-09 13:46:58 -0300

    front branch

--- client/src/components/PinDetail.tsx
+++ client/src/components/PinDetail.tsx
@@ -7,7 +7,7 @@ import { client, urlFor } from '../utils/client'
 import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data'
 import { MasonryLayout, Spinner } from "./"
 
-import { User, PinDetail, Comment, PinType, PostedBy } from '../@types'
+import { User, PinDetails, Comment, PinType, PostedBy } from '../@types'
 
 interface PropsPinDetail {
   user: User;
@@ -15,7 +15,7 @@ interface PropsPinDetail {
 
 const PinDetail = ({user}: PropsPinDetail) => {
   const [pins, setPins] = useState<PinType[] | null>(null)
-  const [pinDetail, setPinDetail] = useState<PinDetail | null>(null)
+  const [pinDetails, setPinDetails] = useState<PinDetails | null>(null)
   const [comment, setComment] = useState("")
   const [addingComment, setAddingComment] = useState(false)
   const { pinId } = useParams()
@@ -27,7 +27,7 @@ const PinDetail = ({user}: PropsPinDetail) => {
 
     client.fetch(query)
       .then((data) => {
-        setPinDetail(data[0])
+        setPinDetails(data[0])
 
         if(data[0]) {
           query = pinDetailMorePinQuery(data[0])
@@ -64,7 +64,7 @@ const PinDetail = ({user}: PropsPinDetail) => {
     fetchPinDetails()
   }, [pinId])
 
-  if(!pinDetail) {
+  if(!pinDetails) {
     return <Spinner message="Loading pin..." />
   }
 
@@ -72,13 +72,13 @@ const PinDetail = ({user}: PropsPinDetail) => {
     <>
     <section className="rounded-[32px] flex xl-flex-row flex-col m-auto bg-white max-w-[1500px]">
       <div className="flex justify-center items-center md:items-start flex-initial">
-        <img src={pinDetail?.image && urlFor(pinDetail.image).url()} alt="user-post" className="rounded-t-3xl rounded-b-lg" />
+        <img src={pinDetails?.image && urlFor(pinDetails.image).url()} alt="user-post" className="rounded-t-3xl rounded-b-lg" />
       </div>
       <div className="w-full p-5 flex-1 xl:min-w-620">
         <div className="flex items-center justify-between">
           <div className="flex gap-2 items-center">
             <a 
-              href={`${pinDetail.image?.asset?.url}?dl=`} 
+              href={`${pinDetails.image?.asset?.url}?dl=`} 
               download
               onClick={(e) => e.stopPropagation()}
               className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
@@ -86,22 +86,22 @@ const PinDetail = ({user}: PropsPinDetail) => {
                 <MdDownloadForOffline />
               </a>
           </div>
-          <a className="underline hover:text-gray-600 transition-all duration-200" href={`${pinDetail.destination}`} target="_blank">{pinDetail.destination}</a>
+          <a className="underline hover:text-gray-600 transition-all duration-200" href={`${pinDetails.destination}`} target="_blank">{pinDetails.destination}</a>
         </div>
 
         <div>
-          <h1 className="text-4xl font-bold break-words mt-3">{pinDetail.title}</h1>
-          <p className="mt-3">{pinDetail.about}</p>
+          <h1 className="text-4xl font-bold break-words mt-3">{pinDetails.title}</h1>
+          <p className="mt-3">{pinDetails.about}</p>
         </div>
 
-        <Link className="flex gap-2 mt-5 items-center bg-white rounded-lg" to={`/user-profile/${pinDetail.postedBy?._id}`}>
-        <img className="w-8 h-8 rounded-full object-cover" src={pinDetail.postedBy?.image} alt="user-profile" />
-        <p className="font-semibold capitalize">{pinDetail.postedBy?.username}</p>
+        <Link className="flex gap-2 mt-5 items-center bg-white rounded-lg" to={`/user-profile/${pinDetails.postedBy?._id}`}>
+        <img className="w-8 h-8 rounded-full object-cover" src={pinDetails.postedBy?.image} alt="user-profile" />
+        <p className="font-semibold capitalize">{pinDetails.postedBy?.username}</p>
         </Link>
         
         <h2 className="mt-5 text-2xl">Comments</h2>
         <div className="max-h-370 overflow-y-auto">
-          {pinDetail.comments && pinDetail.comments.map((comment, i) => (
+          {pinDetails.comments && pinDetails.comments.map((comment, i) => (
             <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={i}>
               <img src={comment.postedBy.image} alt="user-profile comment" className="h-10 w-10 rounded-full cursor-pointer" />
               <div className="flex flex-col">
@@ -113,7 +113,7 @@ const PinDetail = ({user}: PropsPinDetail) => {
         </div>
 
         <div className="flex flex-wrap mt-6 gap-3">
-          <Link className="" to={`/user-profile/${pinDetail.postedBy?._id}`}>
+          <Link className="" to={`/user-profile/${pinDetails.postedBy?._id}`}>
           <img className="w-10 h-10 rounded-full cursor-pointer" src={user.image} alt="user-profile" />
           </Link>
           <input 
