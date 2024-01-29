0469988800f7e0a7b28bef2d17958d961ced8ed2
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2024-01-29 08:49:14 -0300

    feat: Add loader component

---
+++ client/src/components/Loader.tsx
@@ -0,0 +1,10 @@
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center w-full">
+        <img src="/loader.svg" alt="Loader" width={60} height={60} />
+    </div>
+  )
+}
+
+export default Loader
